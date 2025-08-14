16a88c822e994ab8731cce3b09e55f59d516e850
harshv06 <vishwakarmah45@gmail.com>
2025-08-14 17:56:14 +0530

    Added controllers,service,dto,filters and security config for JWT authentication

---
+++ src/main/java/com/consultadd/taskmanager/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.consultadd.taskmanager.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(csrf->csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        req->req
+                                .requestMatchers("/api/v1/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(
+                        session->session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider());
+                return httpSecurity.build();
+    }
+}
--- src/main/java/com/consultadd/taskmanager/controller/AuthorizationController.java
+++ src/main/java/com/consultadd/taskmanager/controller/AuthorizationController.java
@@ -1,6 +1,7 @@
 package com.consultadd.taskmanager.controller;
 
 import com.consultadd.taskmanager.dto.LoginRequestDTO;
+import com.consultadd.taskmanager.dto.RefreshTokenRequestDTO;
 import com.consultadd.taskmanager.dto.RegisterRequestDTO;
 import com.consultadd.taskmanager.service.AuthorizationService;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/auth")
 public class AuthorizationController {
     private final AuthorizationService authorizationService;
     public AuthorizationController(AuthorizationService authorizationService){
@@ -25,6 +26,10 @@ public class AuthorizationController {
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequestDTO loginRequestDTO){
         return ResponseEntity.ok("User logged in successfully");
+    }
 
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refreshToken(@RequestBody RefreshTokenRequestDTO refreshTokenRequestDTO){
+        return ResponseEntity.ok("Refresh Token");
     }
 }
